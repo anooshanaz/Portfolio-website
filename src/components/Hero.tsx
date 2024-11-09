13c614c1611100d1d7519de8bb63b8bb46656f1f
Anoosha Naz <163389690+anooshanaz@users.noreply.github.com>
2024-11-09 12:01:59 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,12 @@ import Navbar from './Navbar'
 
 const Hero = () => {
   return (
-    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/images.jpg)] bg-left lg:bg-[15%] bg-cover'
+    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/images1.png)] bg-left lg:bg-[15%] bg-cover sm:bg-[50%]'
     style={{backgroundSize:"35%"}}>
       <Navbar/>
       <div className='container grid lg:grid-cols-2 h-[calc(100vh-600px)]'>
         <div className='hidden lg:block'></div>
-        <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center mt-20'>
+        <div className='text-[60px] sm:text-[80px] font-bold leading-tight flex justify-center items-center mt-20'>
           <div>
             <p data-aos="zoom-in-up" className='text-cyan-600'>I'm</p>
             <p data-aos="zoom-in-up" className='text-white'>Anoosha</p>
